6e2f7357105b56bc004743e256a4850e40b3c177
Tejas4895 <patiltejas.tsp@gmail.com>
2021-03-27 18:20:45 +0530

    Day 2: Added text file

--- producer_consumer/producer/main.c
+++ producer_consumer/producer/main.c
@@ -3,7 +3,9 @@
 int main(void)
 {
     fprintf(stdout,"In %s file, at %d line ", __FILE__, __LINE__);
-    fprintf(stdout, "\n\n");
+   //todo:
+  
+   fprintf(stdout, "\n\n");
 return EXIT_SUCCESS;
 
 }
